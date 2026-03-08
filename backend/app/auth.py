db791db03d4fe29f15a2b565d479392808f0db93
Joachim-wachira <caesarjoakim@gmail.com>
2026-03-08 14:53:34 +0300

    Update auth.py

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_identity
 from .models import User
